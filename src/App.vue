7bfe071065ee80b5f81f72a560d8b827baf70ca5
Simon He <57086651+Simon-He95@users.noreply.github.com>
2025-02-02 22:56:14 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="dark:text-gray-200 text-gray-700 text-center py-10 px-4 font-sans">
+    <router-view />
+    <Footer />
+  </main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const name = ref('')
+
+const router = useRouter()
+function go() {
+  if (name.value)
+    router.push(`/hi/${encodeURIComponent(name.value)}`)
+}
+</script>
+
+<template>
+  <div>
+    <div class="text-4xl inline-block" i-carbon-campsite />
+    <p>
+      <a
+        rel="noreferrer"
+        href="https://github.com/Simon-He95/vitesse-lite"
+        target="_blank"
+        class="bg-amber-100"
+      >
+        Vitesse Lite
+      </a>
+    </p>
+    <p>
+      <em class="text-sm op75">Opinionated Vite Starter Template</em>
+    </p>
+
+    <div class="py-4" />
+
+    <input
+      id="input"
+      v-model="name"
+      class="border-dark:gay-700 active:outline-none outline-none border-gray-200 border-solid rounded bg-transparent text-center w-[250px] px-4 py-2"
+      placeholder="What's your name?"
+      type="text"
+      autocomplete="false"
+      @keydown.enter="go"
+    >
+
+    <div>
+      <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+        Go
+      </button>
+    </div>
+  </div>
+</template>
